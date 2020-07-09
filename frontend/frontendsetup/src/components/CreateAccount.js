37d144061a8b3b6e142379973c2c09704bc8c2f8
Amy Luo <amyluo@Amys-MacBook-Air.local>
2020-07-09 18:33:38 +1000

    repurposed login and create account, login should be done, create account still needs tweaking

--- frontend/frontendsetup/src/components/CreateAccount.js
+++ frontend/frontendsetup/src/components/CreateAccount.js
@@ -1,6 +1,9 @@
 import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './stylesheet/create.css'
+
+
 class CreateAccount extends Component {
   constructor(props) {
     //allow access to all the component attributes and methods
@@ -37,58 +40,118 @@ class CreateAccount extends Component {
   //create textfields to allow the user to enter in their details 
   render() {
     return (
-      <div>
-          <br/>
-          <br/>
-           First Name:
-            <input
-              placeholder="First Name"
-              onChange={(event, newValue) => this.setState({ first_name: newValue })}
-            />
-            <br/>
-            <br/>
-            Last Name:
-            <input
-              placeholder="Last Name"
-              onChange={(event, newValue) => this.setState({ last_name: newValue })}
-            />
-            <br />
-            <br/>
-            Email Address:
-            <input
-              placeholder="Enter your Email"
-              type="email"
-              onChange={(event, newValue) => this.setState({ email: newValue })}
-            />
-            <br />
-            <br/>
-            State
-            <input
-              placeholder="Select your State"
-              onChange={(event, newValue) => this.setState({ state: newValue })}
-            />
-            <br />
-            <br/>
-            Postcode: 
-            <input
-              placeholder="Enter your Postocde"
-              onChange={(event, newValue) => this.setState({ postcode: newValue })}
-            />
-            <br />
-            <br/>
-            Password: 
-            <input
-              type="password"
-              placeholder="Enter your Password"
-              onChange={(event, newValue) => this.setState({ password: newValue })}
-            />
-            <br />
-            <br/>
-            <button> Submit </button> 
-            <br/>
-            <br/>
-            Already have an Account <Link to="/LogInAcc">Sign In Now</Link>
-      </div>
+      // <div>
+      //     <br/>
+      //     <br/>
+      //      First Name:
+      //       <input
+      //         placeholder="First Name"
+      //         onChange={(event, newValue) => this.setState({ first_name: newValue })}
+      //       />
+      //       <br/>
+      //       <br/>
+      //       Last Name:
+      //       <input
+      //         placeholder="Last Name"
+      //         onChange={(event, newValue) => this.setState({ last_name: newValue })}
+      //       />
+      //       <br />
+      //       <br/>
+      //       Email Address:
+      //       <input
+      //         placeholder="Enter your Email"
+      //         type="email"
+      //         onChange={(event, newValue) => this.setState({ email: newValue })}
+      //       />
+      //       <br />
+      //       <br/>
+      //       State
+      //       <input
+      //         placeholder="Select your State"
+      //         onChange={(event, newValue) => this.setState({ state: newValue })}
+      //       />
+      //       <br />
+      //       <br/>
+      //       Postcode: 
+      //       <input
+      //         placeholder="Enter your Postocde"
+      //         onChange={(event, newValue) => this.setState({ postcode: newValue })}
+      //       />
+      //       <br />
+      //       <br/>
+      //       Password: 
+      //       <input
+      //         type="password"
+      //         placeholder="Enter your Password"
+      //         onChange={(event, newValue) => this.setState({ password: newValue })}
+      //       />
+      //       <br />
+      //       <br/>
+      //       <button> Submit </button> 
+      //       <br/>
+      //       <br/>
+      //       Already have an Account <Link to="/LogInAcc">Sign In Now</Link>
+      // </div>
+
+      <>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+        <div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+			    		<h3 class="panel-title">Please sign up<small>It's free!</small></h3>
+			 			</div>
+			 			<div class="panel-body">
+			    		<form role="form">
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+                      <input name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name" 
+                          onChange={(event, newValue) => this.setState({ first_name: newValue })} />
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+                      <input name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name" 
+                          onChange={(event, newValue) => this.setState({ last_name: newValue })}/>
+			    					</div>
+			    				</div>
+			    			</div>
+
+			    			<div class="form-group">
+                  <input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address" 
+                          onChange={(event, newValue) => this.setState({ email: newValue })}/>
+			    			</div>
+
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password"/>
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password"/>
+			    					</div>
+			    				</div>
+
+			    			</div>
+
+                <input type="submit" id= "register" value="Register" class="btn btn-info btn-block"/>
+			    			
+			    			
+			    		</form>
+			    	</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>
+
+      </>
     );
   }
 }
--- frontend/frontendsetup/src/components/LogInAcc.js
+++ frontend/frontendsetup/src/components/LogInAcc.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import './stylesheet/login.css'
+
 class LogInAcc extends Component {
   constructor(props){
   super(props);
@@ -12,30 +14,73 @@ class LogInAcc extends Component {
 
 render() {
     return (
-      <div>
-        <p></p>
-        <p></p>
-        <p></p>
-            Email Address
-           <input
-             placeholder="Email"
-             onChange = {(event,newValue) => this.setState({email:newValue})}
-             />
-           <br/>
-           <br/>
-              Password
-             <input
-               placeholder="Password"
-               type = "password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>             
-             <br/>
-             <button>Log In </button>
-             <br/>            
-            <br/>
-             Don't have Account <Link to="/CreateAccount">Register Now</Link>
+      // <div id = "comps">
+      //   <p></p>
+      //   <p></p>
+      //   <p></p>
+      //       Email address
+      //      <input
+      //        placeholder="Email"
+      //        onChange = {(event,newValue) => this.setState({email:newValue})}
+      //        />
+      //      <br/>
+      //      <br/>
+      //         Password
+      //        <input
+      //          placeholder="Password"
+      //          type = "password"
+      //          onChange = {(event,newValue) => this.setState({password:newValue})}
+      //          />
+      //        <br/>             
+      //        <br/>
+      //        <button>Log In </button>
+      //        <br/>            
+      //       <br/>
+      //        Don't have Account <Link to="/CreateAccount">Register Now</Link>
+      // </div>
+      <>
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/> 
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <div class="wrapper fadeInDown">
+        <div id="formContent">
+
+          
+          {/* uncomment after */}
+          {/* <div class="fadeIn first">
+              <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+          </div> */}
+
+          <form>
+            <input type="text" id="login" class="fadeIn second" name="login" placeholder="login" 
+                        onChange = {(event,newValue) => this.setState({email:newValue})}/> 
+            <input type="password" id="password" class="fadeIn third" name="login" placeholder="password"
+                        onChange = {(event,newValue) => this.setState({password:newValue})}/>
+            <a href="#">Forgot your password?</a>
+            <br></br>
+            <br></br>
+            <input type="submit" class="fadeIn fourth" value="Log In"/>
+            <br></br>
+            
+            <div id ="remember">
+              remember me &nbsp;&nbsp;
+              <input type="checkbox"/>
+            </div>
+					
+	
+          </form>
+
+          <div id="formFooter">
+      
+            Don't have an account<br></br>
+            <a class="underlineHover" href="/CreateAccount">Register Now</a>
+          </div>
+
+          
+        </div>
       </div>
+      </>
+    
     );
   }
 }
